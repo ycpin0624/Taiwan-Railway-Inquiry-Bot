d925b8d52843342b47b4c5b476ac0b024ab91229
ycpin <yangpokemonpin@gmail.com>
2021-09-02 22:49:21 +0800

    2021-09-02 update

--- trainInfo/trainQuery.py
+++ trainInfo/trainQuery.py
@@ -59,7 +59,7 @@ def trainQuery(start_station, end_station, ride_date, start_time, end_time):
                     continue
 
                 spamwriter.writerow(
-                    [ride_date, train_number, departure_time, arrive_time, trip_type, booking])
+                    [train_type + ' ' + ride_date, train_number, departure_time, arrive_time, trip_type, booking])
 
     # format data
     data = pd.read_csv('trainInfo/trainData.csv', engine='python')
